098a93def0c1376c4fa0eefb815d526e9a886c87
Eliton lima <elitonlima2@hotmail.com>
2022-10-10 18:04:14 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandowebcouse/config/TestConfig.java
@@ -0,0 +1,34 @@
+package com.educandowebcouse.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandowebcouse.entities.User;
+import com.educandowebcouse.repositories.UserRepository;
+
+@Configuration
+@Profile("test") // configuração especifica para o perfil de teste, esta na
+					// application.properties
+public class TestConfig implements CommandLineRunner {
+// CommandLineRunner executa quando o programa for iniciado
+// serve para popular o banco de dados com alguns objetos, e fazer a injeção de depêndencias no UserRepository
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+// null pois o id vai ser gerado pelo banco de dados	
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+// salva uma lista de objetos no banco
+		userRepository.saveAll(Arrays.asList(u1,u2));
+
+	}
+
+}
---
+++ src/main/java/com/educandowebcouse/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.educandowebcouse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandowebcouse.entities.User;
+// a classe Repository salva os dados
+// UserRepository é um objeto Repository que tem várias operações para trabalhar com o User
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
