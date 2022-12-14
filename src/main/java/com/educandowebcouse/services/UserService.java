6dd8b5fd612ad0508ac82670889cb4f66f483a7b
Eliton lima <elitonlima2@hotmail.com>
2022-12-14 14:21:56 -0300

    Exception handling - update

--- src/main/java/com/educandowebcouse/services/UserService.java
+++ src/main/java/com/educandowebcouse/services/UserService.java
@@ -3,6 +3,8 @@ package com.educandowebcouse.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,53 +15,54 @@ import com.educandowebcouse.repositories.UserRepository;
 import com.educandowebcouse.services.exceptions.DatabaseException;
 import com.educandowebcouse.services.exceptions.ResourceNotFoundException;
 
- @Service   // classe de serviço do Spring é um componente
+@Service // classe de serviço do Spring é um componente
 public class UserService {
 
 	@Autowired // injeção de dependências
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	// Optional<User> retorna um objeto
-	// tenta dar o get se não tiver usuário lança exceção |orElseThrow(() -> new ResourceNotFoundException(id))
+	// tenta dar o get se não tiver usuário lança exceção |orElseThrow(() -> new
+	// ResourceNotFoundException(id))
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));// retorna o objeto
 	}
-	
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
-	
-	
-	
+
 }
