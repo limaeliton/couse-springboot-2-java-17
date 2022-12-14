25301e56cf6d761d0d1c64d0cdcff3523e0f4aec
Eliton lima <elitonlima2@hotmail.com>
2022-12-14 11:59:45 -0300

    Exception handling - delete

--- src/main/java/com/educandowebcouse/services/UserService.java
+++ src/main/java/com/educandowebcouse/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandowebcouse.entities.User;
 import com.educandowebcouse.repositories.UserRepository;
+import com.educandowebcouse.services.exceptions.DatabaseException;
 import com.educandowebcouse.services.exceptions.ResourceNotFoundException;
 
  @Service   // classe de serviço do Spring é um componente
@@ -33,7 +36,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
